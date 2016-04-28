bbfa89ea99f1140902d31bfce5bbfc0b7642857d
USER <USER@SAKE-PC>
2016-04-28 19:17:49 -0300

    Teste1

---
+++ src/br/edu/unidavi/gamepong/Cliente.java
@@ -0,0 +1,51 @@
+package br.edu.unidavi.gamepong;
+
+
+
+import br.edu.unidavi.gamepong.Recebedor;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * 
+ */
+public class Cliente {
+   public static void main(String[] args) 
+         throws UnknownHostException, IOException {
+     
+     // dispara cliente
+     new Cliente("10.3.4.16", 12345).executa();
+   }
+   
+   private String host;
+   private int porta;
+   
+   public Cliente (String host, int porta) {
+     this.host = host;
+     this.porta = porta;
+   }
+   
+   public void executa() throws UnknownHostException, IOException {
+     Socket cliente = new Socket(this.host, this.porta);
+     System.out.println("Cliente Conectado!");
+ 
+     //thread que recebe mensagens do servidor
+     Recebedor r = new Recebedor(cliente.getInputStream());
+     new Thread(r).start();
+     
+     // Lê as mensagens e manda para o servidor.
+     Scanner teclado = new Scanner(System.in);
+     PrintStream saida = new PrintStream(cliente.getOutputStream());
+     while (teclado.hasNextLine()) {
+       saida.println(teclado.nextLine());
+     }
+     
+     saida.close();
+     teclado.close();
+     cliente.close();    
+   }
+ }
---
+++ src/br/edu/unidavi/gamepong/TrataCliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.unidavi.gamepong;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * 
+ */
+public class TrataCliente implements Runnable {
+ 
+   private InputStream cliente;
+   private Servidor servidor;
+ 
+   public TrataCliente(InputStream cliente, Servidor servidor) {
+     this.cliente = cliente;
+     this.servidor = servidor;
+   }
+ 
+   public void run() {
+     
+     Scanner s = new Scanner(this.cliente);
+     while (s.hasNextLine()) {
+       servidor.distribuiMensagem(s.nextLine());
+     }
+     s.close();
+   }
+ }
